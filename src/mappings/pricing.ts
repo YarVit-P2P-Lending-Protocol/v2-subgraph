df0989a04377b39ca1c3b3688547e55295117884
Oleksii Shulzhenko <oleksii.shulzhenko@ambisafe.com>
2024-05-19 20:18:33 +0300

    arbitrum address fixed

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -4,10 +4,10 @@ import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts/index'
 import { Bundle, Pair, Token } from '../types/schema'
 import { ADDRESS_ZERO, factoryContract, ONE_BD, UNTRACKED_PAIRS, ZERO_BD } from './helpers'
 
-const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
-const USDC_WETH_PAIR = '0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc' // created 10008355
-const DAI_WETH_PAIR = '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11' // created block 10042267
-const USDT_WETH_PAIR = '0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852' // created block 10093341
+const WETH_ADDRESS = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
+const USDC_WETH_PAIR = '0xF64Dfe17C8b87F012FCf50FbDA1D62bfA148366a' // created 10008355
+const DAI_WETH_PAIR = '0x8dca5a5DBA32cA529594d43F86ED4210EaD2Ed83' // created block 10042267
+const USDT_WETH_PAIR = '0xd04Bc65744306A5C149414dd3CD5c984D9d3470d' // created block 10093341
 
 export function getEthPriceInUSD(): BigDecimal {
   // fetch eth prices for each stablecoin
@@ -41,27 +41,20 @@ export function getEthPriceInUSD(): BigDecimal {
 
 // token where amounts should contribute to tracked volume and liquidity
 let WHITELIST: string[] = [
-  '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
-  '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
-  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
-  '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
-  '0x0000000000085d4780b73119b644ae5ecd22b376', // TUSD
-  '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643', // cDAI
-  '0x39aa39c021dfbae8fac545936693ac917d5e7563', // cUSDC
-  '0x86fadb80d8d2cff3c3680819e4da99c10232ba0f', // EBASE
-  '0x57ab1ec28d129707052df4df418d58a2d46d5f51', // sUSD
-  '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', // MKR
-  '0xc00e94cb662c3520282e6f5717214004a7f26888', // COMP
-  '0x514910771af9ca656af840dff83e8264ecf986ca', //LINK
-  '0x960b236a07cf122663c4303350609a66a7b288c0', //ANT
-  '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', //SNX
-  '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', //YFI
-  '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', // yCurv
-  '0x853d955acef822db058eb8505911ed77f175b99e', // FRAX
-  '0xa47c8bf37f92abed4a126bda807a7b7498661acd', // WUST
-  '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // UNI
-  '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC
-  '0x956f47f50a910163d8bf957cf5846d573e7f87ca', // FEI
+  '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1', // WETH
+  '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1', // DAI
+  '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', // USDC
+  '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9', // USDT
+  '0x4D15a3A2286D883AF0AA1B3f21367843FAc63E07', // TUSD
+  '0xA970AF1a584579B618be4d69aD6F73459D112F95', // sUSD
+  '0x354A6dA3fcde098F8389cad84b0182725c6C91dE', // COMP
+  '0xf97f4df75117a78c1A5a0DBb814Af92458539FB4', //LINK
+  '0x3082CC23568eA640225c2467653dB90e9250AaA0', //ANT
+  '0x82e3A8F066a6989666b031d916c43672085b1582', //YFI
+  '0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F', // FRAX
+  '0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0', // UNI
+  '0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f', // WBTC
+  '0xBfAE6fecD8124ba33cbB2180aAb0Fe4c03914A5A', // FEI
 ]
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
